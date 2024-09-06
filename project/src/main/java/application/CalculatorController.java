8ec80cd0dc3b211a81eb44d677e0b5574833d06c
angelesmarin <angeles.marinbatana@bsu.edu>
2024-09-05 23:09:30 -0400

    Add divide by zero test; Add calculateResult method in CalculatorController

--- project/src/main/java/application/CalculatorController.java
+++ project/src/main/java/application/CalculatorController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.ToggleGroup;
 
 public class CalculatorController {
 	boolean showBaseFourValue = true;
+	Calculator calculator = new Calculator();
+
 
 	@FXML
 	private Label displayLabel;
@@ -23,6 +25,22 @@ public class CalculatorController {
 	@FXML
 	protected void onNegativeButtonPress(){
 
+	}
+	private String calculateResult(String num1, String num2, String operator) {
+		switch(operator) {
+			case "+":
+				return calculator.sumOfNumbers(num1, num2);
+			case "-":
+				return  calculator.differenceOfNumbers(num1, num2);
+			case "/":
+				return calculator.multiplicationOfNumbers(num1, num2);
+			case "√":
+				return calculator.squareRootBaseFourNumber(num1);
+			case "^2":
+				return calculator.squareBaseFourNumber(num1);
+			default:
+				return "try again";
+		}
 	}
 
 	@FXML
